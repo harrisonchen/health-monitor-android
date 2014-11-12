8011ce4b4af007f1bc40d4f5fc5533d876cc42df
Jeffrey C <chen.jeffrey.r@gmail.com>
2014-11-12 15:44:33 -0800

    Bluetooth now working through SoftwareSerial

--- Bluetooth Module/softwareSerial/softwareSerial.ino
+++ Bluetooth Module/softwareSerial/softwareSerial.ino
@@ -1,20 +1,42 @@
+/*
+  Software serial multple serial test
+ 
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+ 
+ The circuit: 
+ * RX is digital pin 10 (connect to TX of other device)
+ * TX is digital pin 11 (connect to RX of other device)
+ 
+ Not all pins on the Leonardo support change interrupts, 
+ so only the following can be used for RX: 
+ 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+ */
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(1,0);
+SoftwareSerial mySerial(10, 11); // RX, TX
 
-void setup() {
-Serial.begin(9600);
-mySerial.begin(9600);
-delay(5000);
-Serial.println("go");
+void setup()  
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(57600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+
+  Serial.println("Goodnight moon!");
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+  mySerial.println("Hello, world?");
 }
 
-void loop() {
-  int data;
-  if(mySerial.available()){
-    data = mySerial.read();
-  delay(20);
-  Serial.println(data);
-  } 
+void loop() // run over and over
+{
+  if (mySerial.available())
+    Serial.write(mySerial.read());
+  if (Serial.available())
+    mySerial.write(Serial.read());
 }
 
