f635c66251e60f34b3228aa7da534f7712d11038
Jeffrey C <chen.jeffrey.r@gmail.com>
2014-12-09 14:15:19 -0800

    updated names and readme, also added temp_sense_2.0 using OneWire library, which is now fully working

--- Individual Modules/Temperature/temp_sense_1.5/temp_sense_serial_1.5.ino
+++ Individual Modules/Temperature/temp_sense_1.5/temp_sense_serial_1.5.ino
@@ -3,7 +3,7 @@
 SoftwareSerial mySerial(10, 11); // RX, TX
 
 //TMP36 Pin Variables
-int sensorPin = 1; //Says we are working with a sensor in analog pin 0
+int sensorPin = 2; //Says we are working with a sensor in analog pin 0
 /*
 * setup() - this function runs once when you turn your Arduino on
  * We initialize the serial connection with the computer
@@ -15,6 +15,7 @@ void setup()
   Serial.begin(9600); //Start the serial connection with the computer
   mySerial.begin(9600);
   mySerial.println("Hello, world?");
+  pinMode(sensorPin, INPUT);
   //to view the result open the serial monitor 
 }
 void loop() // run over and over againx
@@ -25,27 +26,32 @@ void loop() // run over and over againx
   float voltage = reading * 5.0;
   voltage /= 1024.0; // print out the voltage to the serial port
 
-  float temperature = log(((10240000/reading) - 10000));
+  double temperature = log(((10240000/reading) - 10000));
   temperature = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * temperature * temperature ))* temperature );
   temperature = temperature - 273.15;
 
-  float farenheit = temperature * 9.0 / 5.0 + 32.0;
+  double farenheit = temperature * 9.0 / 5.0 + 32.0;
   delay(1000); //waiting a second
 
   if (mySerial.available()){
     mySerial.print(temperature);
     mySerial.print(" C / ");
-     mySerial.print(farenheit);
-    mySerial.println(" F");
+    mySerial.print(farenheit);
+    mySerial.print(" F / ");
+    mySerial.print(reading);
+    mySerial.println(" ADC");
   }
   if(Serial.available()){
     Serial.print(temperature);
     Serial.print(" C / ");
     Serial.print(farenheit);
-    Serial.println(" F");
+    Serial.print(" F / ");
+    Serial.print(reading);
+    Serial.println(" ADC");
   }
 
 }
+
 
 
 
