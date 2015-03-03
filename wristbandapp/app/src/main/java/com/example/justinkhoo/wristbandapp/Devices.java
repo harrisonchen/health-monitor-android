358eb24f3f5f82258674af5b38bab4d23c4f82d8
Harrison Chen <harrisonxchen@gmail.com>
2015-03-03 13:01:57 -0800

    bluetoothh power on

--- wristbandapp/app/src/main/java/com/example/justinkhoo/wristbandapp/Devices.java
+++ wristbandapp/app/src/main/java/com/example/justinkhoo/wristbandapp/Devices.java
@@ -1,43 +1,137 @@
 package com.example.justinkhoo.wristbandapp;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class Devices extends Activity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    ListView deviceListView;
+    ArrayAdapter<String> deviceArrayAdapter;
+    ArrayList<BluetoothDevice> deviceList;
+    ArrayList<String> arrayList;
+
+    // Create a BroadcastReceiver for ACTION_FOUND
+    final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+//            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+//                // Get the BluetoothDevice object from the Intent
+//                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+//                deviceList.add(device);
+//                // Add the name and address to an array adapter to show in a ListView
+//                deviceArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+//                deviceArrayAdapter.notifyDataSetChanged();
+//            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_devices);
 
-        new Handler().postDelayed(new Runnable() {
+        deviceList = new ArrayList<BluetoothDevice>();
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
+        deviceListView = (ListView) findViewById(R.id.deviceListView);
+
+        arrayList = new ArrayList<String>();
+        arrayList.add("Example Device");
+        deviceArrayAdapter = new ArrayAdapter<String>(this, R.layout.custom_lobby_row, arrayList);
+
+        deviceListView.setAdapter(deviceArrayAdapter);
+
+        if(bluetoothAdapter != null) {
+            // Register the BroadcastReceiver
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(receiver, filter); // Don't forget to unregister during onDestroy
+
+            if(!bluetoothAdapter.isEnabled()) {
+                connectBluetooth();
+                discover();
+            }
+            else {
+                discover();
+            }
+        }
+
+        deviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                bluetoothAdapter.cancelDiscovery();
+                Intent intent = new Intent(Devices.this, Lobby.class);
+                startActivity(intent);
+
+                finish();
+
+                overridePendingTransition(R.anim.slide_down_in, R.anim.slide_down_out);
+            }
+        });
+    }
+
+    public void connectBluetooth() {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    }
+
+    public void discover(){
+        if(bluetoothAdapter.isDiscovering()) {
+            bluetoothAdapter.cancelDiscovery();
+            Toast.makeText(getApplicationContext(), "Bluetooth already discovering.\nStopping discovery...", Toast.LENGTH_LONG).show();
+            Log.d("Start Discovery: ", "already discovering");
+        }
+        else if(bluetoothAdapter.startDiscovery()) {
+            Toast.makeText(getApplicationContext(),"Bluetooth discovering" , Toast.LENGTH_LONG).show();
+            Log.d("Start Discovery: ", "true");
+//            deviceArrayAdapter.clear();
+        }
+    }
+
+    public void manualTransitionOut() {
+        new Handler().postDelayed(new Runnable() {
 
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
                 Intent i = new Intent(Devices.this, Lobby.class);
                 startActivity(i);
 
-                // close this activity
                 finish();
 
                 overridePendingTransition(R.anim.slide_up_in, R.anim.slide_up_out);
             }
-        }, 1500);
-
+        }, 1000);
     }
 
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        bluetoothAdapter.cancelDiscovery();
+        unregisterReceiver(receiver);
+        Log.d("ACTIVITY1 ON DESTROY: ", "CALLED");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
